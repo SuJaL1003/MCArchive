2d1989d6a642e774881dea9dd1f6a57c2ee5b484
SuJaL1003 <99916468+SuJaL1003@users.noreply.github.com>
2025-06-16 20:48:29 +0530

    Fix

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Layout from "../components/Layout/Layout";
 import PDFUploadForm from "../components/PDFUploadForm";
-import PYQUploadForm from "../components/PyqUploadForm";
+import PYQUploadForm from "../components/PYQUploadForm";
 
 const AdminDashboard = () => {
   const [semester, setSemester] = useState("");
